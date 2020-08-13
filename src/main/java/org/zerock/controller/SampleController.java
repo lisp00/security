da75d924cbf6d1bbb46bbb1dc96b7e845db58606
user <user@DESKTOP-JMO5NOD>
2020-08-13 19:11:10 +0900

    11

---
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,28 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+//샘플 컨트롤러
+@Log4j
+@RequestMapping("/sample/*")
+@Controller
+public class SampleController {
+	@GetMapping("/all")
+	public void doAll() {
+		log.info("do allcan access everybody");
+	}
+	
+	@GetMapping("/member")
+	public void doMember() {
+		log.info("logined member");
+	}
+	
+	@GetMapping("/admin")
+	public void doAdmin() {
+		log.info("admin only");
+	}
+}
---
+++ src/main/java/org/zerock/security/CustomLoginSuccessHandler.java
@@ -0,0 +1,42 @@
+package org.zerock.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import lombok.extern.log4j.Log4j;
+
+//로그인성공시 핸들러 커스텀. 역할에 따라 로그인 이후 즉시 리다이렉션
+@Log4j
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		log.warn("Login Success");
+		
+		List<String> roleNames = new ArrayList<String>();
+		
+		authentication.getAuthorities().forEach(authority -> {
+			roleNames.add(authority.getAuthority());
+		});
+		
+		log.warn("ROLE NAME : " + roleNames);
+		
+		if(roleNames.contains("ROLE_ADMIN")) {
+			response.sendRedirect("/sample/admin");
+			return;
+		}
+		
+		if(roleNames.contains("ROLE_MEMBER")) {
+			response.sendRedirect("/sample/member");
+			return;
+		}
+		response.sendRedirect("/");
+	}
+}
